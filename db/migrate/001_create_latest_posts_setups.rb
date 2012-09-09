54de6fa6dc90475113a14f0248f934b7dec81bc6
root <root@mail.gogolek.co.uk>
2012-09-09 19:59:39 +0400

    making sure all the naming is the same - cleanup

--- db/migrate/001_create_latest_posts_setups.rb
+++
@@ -1,8 +0,0 @@
-class CreateLatestPostsSetups < ActiveRecord::Migration
-  def change
-    create_table :latest_posts_setups do |t|
-      t.integer :max_count, :default => 5
-      t.string :side, :default => 'left'
-    end
-  end
-end
